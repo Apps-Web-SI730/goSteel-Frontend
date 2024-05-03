d7de8c181212cc61924497adaea41858725999c3
USER <u202119880@upc.udu.pe>
2024-05-03 16:16:37 -0500

    feat(booking.entity):add new entity

--- src/core/fast-scooter/models/booking.entity.js
+++ src/core/fast-scooter/models/booking.entity.js
@@ -1,7 +1,15 @@
 export class Booking {
-    constructor(id, initDate, endDate) {
+    // constructor(id, initDate, endDate) {
+    //     this.id = id;
+    //     this.initDate = initDate;
+    //     this.endDate = endDate;
+    // }
+
+    constructor(id, brand, description, price, image) {
         this.id = id;
-        this.initDate = initDate;
-        this.endDate = endDate;
+        this.brand = brand;
+        this.description = description;
+        this.price = price;
+        this.image = image;
     }
 }
