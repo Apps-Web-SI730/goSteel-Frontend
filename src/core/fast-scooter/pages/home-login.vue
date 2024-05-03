4727b8aecbfed1d0ec04e0932d570c5f8de63d81
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2024-05-03 16:29:32 -0500

    feat: create page home (home-login)

---
+++ src/core/fast-scooter/pages/home-login.vue
@@ -0,0 +1,31 @@
+<script>
+import HomeUs from '@/core/public/components/home-us.component.vue';
+import HomeScooters from '@/core/public/components/home-scooters.component.vue';
+import HomeGetStarted from '@/core/public/components/home-get-started.component.vue'
+import HomeTeam from '@/core/public/components/home-team.component.vue'
+import HomeContact from '@/core/public/components/home-contact.component.vue'
+import FakeToolbar from '@/core/fast-scooter/components/fake-toolbar.vue'
+
+export default {
+  name: 'home-view.component',
+  components: {
+    FakeToolbar,
+
+
+    HomeUs,
+    HomeScooters,
+    HomeGetStarted,
+    HomeTeam,
+    HomeContact,
+  }
+}
+</script>
+
+<template>
+<fake-toolbar/>
+
+</template>
+
+<style scoped>
+
+</style>
