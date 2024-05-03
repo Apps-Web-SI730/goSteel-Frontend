77ac01d57fa63982cdf3c3f35f970a5e2710df30
USER <u202119880@upc.udu.pe>
2024-05-03 16:26:22 -0500

    feat(about-us.component):add a new component that shows our characteristics as a company

---
+++ src/core/public/components/about-us.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+import LoginToolbar from '@/core/fast-scooter/components/login-toolbar.vue'
+</script>
+
+
+<template>
+  <login-toolbar/>
+  <div class="surface-section px-4 py-8 md:px-6 lg:px-8 text-center">
+    <div class="mb-3 font-bold text-3xl">
+      <span class="text-900">About us, </span>
+      <span class="text-orange-500">Discover Who We Are Behind</span>
+    </div>
+
+    <div class="grid">
+      <div class="col-12 md:col-4 mb-4 px-5">
+        <span class="p-4 mb-3 inline-block surface-card" style="border-radius: 10px">
+          <div class="i-container">
+            <i class="fa-solid fa-bicycle" style="color: #f97316"></i>
+          </div>
+        </span>
+        <div class="text-900 text-orange-500 text-xl mb-3 font-medium">Mission</div>
+        <span class="text-700 line-height-3">At GoSteel, our mission is to provide efficient and sustainable mobility solutions for businesses of all sizes. We are committed to offering a business electric scooter rental service, FastScooter, that allows our customers to optimize their commute, reduce operating costs and contribute positively to the environment.</span>
+      </div>
+
+      <div class="col-12 md:col-4 mb-4 px-5">
+        <span class="p-4 mb-3 inline-block surface-card" style="border-radius: 10px">
+          <div class="i-container">
+            <i class="fa-solid fa-person-biking-mountain" style="color: #f97316"></i>
+          </div>
+        </span>
+        <div class="text-900 text-orange-500 text-xl mb-3 font-medium">Vision</div>
+        <span class="text-700 line-height-3">Our vision at GoSteel is to lead the transformation of urban transportation by offering an innovative and eco-friendly alternative to traditional mobility methods. We strive to become strategic partners with commuters, providing them with flexible and customized mobility solutions that drive growth, efficiency and long-term sustainability. </span>
+      </div>
+
+      <div class="col-12 md:col-4 md:mb-4 mb-0 px-3">
+        <span class="p-4 mb-3 inline-block surface-card" style="border-radius: 10px">
+          <div class="i-container">
+            <i class="fa-solid fa-person-biking" style="color: #f97316"></i>
+          </div>
+        </span>
+        <div class="text-900 text-orange-500 text-xl mb-3 font-medium">Values</div>
+        <span class="text-700 line-height-3">Sostenibilidad Ambiental: Nos comprometemos a promover prácticas de movilidad sostenibles y respetuosas con el medio ambiente, contribuyendo activamente a la reducción de emisiones de carbono y la preservación de recursos naturales.
+Innovación Tecnológica: Buscamos constantemente innovar y mejorar nuestros servicios, aprovechando tecnologías avanzadas para ofrecer una experiencia de movilidad eficiente, segura y conveniente.
+Excelencia en el Servicio al Cliente: Priorizamos la satisfacción y el éxito de nuestros clientes, brindándoles un servicio de alta calidad, atención personalizada y soluciones adaptadas a sus necesidades específicas.</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
